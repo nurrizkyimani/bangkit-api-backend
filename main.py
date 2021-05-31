71a7803604aee4c14c876c69ff72c48dc522cffa
nurrizkyimani <nurrizkyimanidev@gmail.com>
2021-05-31 17:33:11 +0700

    add firebase configuration

--- main.py
+++ main.py
@@ -4,15 +4,24 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
+
 cred = credentials.ApplicationDefault()
-
+# cred = credentials.Certificate('charaka-kapi.json')
 firebase_admin.initialize_app(cred, {
-  'projectId' :  os.environ['project_id_py']
+  'projectId' :  'charaka'
 })
-
 db = firestore.client()
+
+
+class Feed(BaseModel):
+  title: str
+  desc: Optional[str] = None
+  book: Optional[str] = None
+
 
 @app.get("/")
 def main_func():
@@ -23,6 +32,7 @@ def main_func():
 def get_user_firebase(user_id: int):
   return {"user_id": user_id, "desc" : "testing is the right thing", "recommendation": [1231231, 123132123, 12312313]}
 
+
 #book api; non firebase
 @app.get("/books/{book_id}")
 def get_book_database(book_id: int):
@@ -30,22 +40,24 @@ def get_book_database(book_id: int):
 
 @app.get("/books/all")
 def get_feed_all_firebase():
-  return { "book_id": book_id, "desc" : [{"title": "yessir"}, {"title": "yessir"}]}
+  return { "book_id": 123123131, "desc" : [{"title": "yessir"}, {"title": "yessir"}]}
+
 
 #feed api; 
 @app.post("/feeds")
-def post_feed_firebase():
+def post_feed_firebase(feed: Feed):
+  
   data = {
-    u'title': u'great book',
-    u'book': u'thinking in bets ',
-    u'description': u'this is coool boook'
+    u'title': feed.title,
+    u'book':  feed.book,
+    u'description': feed.desc
   }
 
   db.collection(u'newsfeed').add(data)
 
-  return {"id": 12313, "desc": "sucesss"}
+  return {"code": 202 , "data": feed }
 
-@app.get("/feeds/{feed_id")
+@app.get("/feeds/{feed_id}")
 def get_feed_firebase(feed_id: int):
   return { "book_id": feed_id, "desc" : [{"title": "goodbook"}, {"title": "not that good book"}]}
 
