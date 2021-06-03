e886d26d05e73bbb706cb0bbfde7c4a4f0692c87
bocahrokok <indieunistudy@gmail.com>
2021-06-03 08:59:54 +0700

    finishing prediction

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from firebase_admin import firestore
 from typing import Optional
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from tensorflow import keras
+import numpy as np
+import googleapiclient.discovery
+import pandas as pd
 
 app = FastAPI()
 
-origins = ["*"]
+origins = ["*",
+           "https://us-central1-charaka.cloudfunctions.net/"
+]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -27,66 +33,103 @@ firebase_admin.initialize_app(cred, {
 db = firestore.client()
 
 class Feed(BaseModel):
+  userid : str
   title: str
   desc: Optional[str] = None
   book: Optional[str] = None
 
 
+
 @app.get("/")
 def main_func():
-  return {"desc": "Helloworld"}
+  return {"desc": "This is Api for charaka deploy in cloud function,"}
 
 #user api
-@app.get("/users/{user_id}")
-def get_user_firebase(user_id: int):
-  return {"user_id": user_id, "desc" : "testing is the right thing", "recommendation": [1231231, 123132123, 12312313]}
-
+# @app.get("/users/{user_id}")
+# def get_user_firebase(user_id: str):
+#   docs = db.collection(u'users').document(user_id).get()
+#   return {"user_id": user_id, "docs": docs }
 
 #book api; non firebase
-@app.get("/books/{book_id}")
-def get_book_database(book_id: int):
-  return { "book_id": book_id, "desc" : { "title": "bias and references"} }
+@app.get("/books/25663888")
+def get_book_database():
+  docs = db.collection(u'books').where(u'book_id', u'==', u'25663888').get()
+  return {"book_id": '25663888', "doc": docs}
 
 @app.get("/books/all")
 def get_feed_all_firebase():
-  return { "book_id": 123123131, "desc" : [{"title": "yessir"}, {"title": "yessir"}]}
+  book_ref = db.collection("books")
+  query = book_ref.limit(5)
+  return {"status": 200, "docs": query}
 
-
-#feed api; 
+#feed api; done 
 @app.post("/feeds")
 def post_feed_firebase(feed: Feed):
   data = {
     u'title': feed.title,
-    u'book':  feed.book,
-    u'description': feed.desc
+    u'description': feed.desc,
+    u'user_id': feed.userid,
+    u'book_id':  feed.book, 
   }
 
   db.collection(u'newsfeed').add(data)
-
   return {"code": 202 , "data": feed }
 
 @app.get("/feeds")
 def get_feed_firebase():
-  docs = db.collection(u'newsfeed').stream()
+  docs = db.collection(u'newsfeed').limit(3).get()
   lis = []
   for doc in docs:
-    print(doc)
-    lis.append({ "id": doc.id, "doc" : doc.to_dict() })
+    bookdoc = db.collection(u'books').where(u'book_id', u'==', u'25663888').get()
+    bookdoc = bookdoc[0]
+    # for doc1 in bookdoc:
+      # print(f'{doc1.id} => {doc1.to_dict()}')
+    lis.append({
+      "id": doc.id,
+      "doc": doc.to_dict(),
+      "bookdoc": bookdoc.to_dict()
+  })
   return lis
 
-@app.get("/feeds/{feed_id}")
-def get_feed_firebase(feed_id: int):
-  return { "feed_id": feed_id, "desc" : [{"title": "goodbook"}, {"title": "not that good book"}]}
+
+@app.get("/feeds/4JH3zzZ3ji9QuZp1ThFV")
+def get_feed_firebase():
+  docs = db.collection(u'newsfeed').document(u'4JH3zzZ3ji9QuZp1ThFV')
+  bookdoc = db.collection(u'books').where(u'book_id', u'==', u'25663888').get()
+  bookdoc = bookdoc[0]
+  return {"status" : 200, "docs": docs, "bookdoc": bookdoc}
 
 
 #prediction api
-@app.get("/predictions/{book_id}")
-def get_book_prediction(book_id: int):
-  return {"book_id": book_id, "recommendation" : [ 1231231, 123123, 12312313] }
+@app.get("/predictions")
+def get_book_prediction():
 
+  url_rating = './ratings.csv'
+  url_books = './books.csv'
+  rating = pd.read_csv(url_rating)
+  
+  books = pd.read_csv(url_books)
+  book_data = np.array(list(set(rating.book_id)))
+  user = np.array([100 for i in range(len(book_data))])
+  model = keras.models.load_model('../bangkit-api-backend')
+  model.summary()
+  
+  predictions = model.predict([[user], [book_data]])
+  predictions = np.array([a[0] for a in predictions])
+  recommended_book_ids = (-predictions).argsort()[:5]
+  
+  bb = books[books['id'].isin(recommended_book_ids)]
 
+  bi = bb.to_dict('index')
+  lit = []
+  for key, value in bi.items():
+    lit.append(value)
+  
+  return {
+    "status": 200,
+    "docs": lit
+    }
 
-#others; 
 
 uvicorn.run(app,host="0.0.0.0",port=8080)
 
